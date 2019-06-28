84f4d69c254fc2bed23979566d47221aab19e4dd
Rafaela Trevizan <rafaela.trevizan09@gmail.com>
2019-06-28 14:27:03 -0300

    28062019

--- ruby/capy/spec/select2_spec.rb
+++ ruby/capy/spec/select2_spec.rb
@@ -9,28 +9,39 @@ describe 'Select2', :select2 do
 
             it 'seleciona ator por nome' do
                 find('.select2-selection--single').click
-                sleep 1
+                
                 find('.select2-results__option', text: 'Adam Sandler').click
-                sleep 5
+                
             end
 
             it 'seleciona ator por nome pela busca' do
                 find('.select2-selection--single').click
-                sleep 1
+                
                 find('.select2-search__field').set('Chris Rock')
-                sleep 1
+                
                 find('.select2-results__option').click
-                sleep 5
+                
             end
 
     end
    
 
-    describe('multiple') do
+    describe('multiple', :multi) do
         
             before(:each) do
                 visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
             end   
+
+            it 'seleciona atores' do
+               find('.select2-selection--multiple').click
+               sleep 1
+            end
+
+            it 'seleciona atores por nome pela busca' do
+                find('.select2-selection--multiple').click
+                
+                
+            end
 
     end
 end
